2bf82c24a4c8cbefa50506663591a481cd21c4af
Zachary Huff <zach.huff.386@gmail.com>
2014-11-29 00:13:26 -0500

    Update build script

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -14,20 +14,15 @@ tar cfz openvpn_$VERSION.orig.tar.gz openvpn-$VERSION
 cp -r ../debian ./openvpn-$VERSION
 cd openvpn-$VERSION
 
+sed -i -e 's/0ubuntu1) unstable;/0ubuntu1~trusty) trusty;/g' debian/changelog
 debuild -S
-sed -i -e 's/0ubuntu1/0ubuntu1~precise/g' debian/changelog
-sed -i -e 's/trusty;/precise;/g' debian/changelog
-debuild -S
-sed -i -e 's/0ubuntu1~precise/0ubuntu1~utopic/g' debian/changelog
-sed -i -e 's/precise;/utopic;/g' debian/changelog
+sed -i -e 's/0ubuntu1~trusty) trusty;/0ubuntu1~precise) precise;/g' debian/changelog
 debuild -S
 
 cd ..
 
 echo '\n\nRUN COMMANDS BELOW TO UPLOAD:'
-echo 'sudo dput ppa:pritunl/ppa ./build/openvpn_'$VERSION'-0ubuntu1_source.changes'
+echo 'sudo dput ppa:pritunl/ppa ./build/openvpn_'$VERSION'-0ubuntu1~trusty_source.changes'
 echo 'sudo dput ppa:pritunl/ppa ./build/openvpn_'$VERSION'-0ubuntu1~precise_source.changes'
-echo 'sudo dput ppa:pritunl/ppa ./build/openvpn_'$VERSION'-0ubuntu1~utopic_source.changes'
-echo 'sudo dput ppa:pritunl/pritunl-testing ./build/openvpn_'$VERSION'-0ubuntu1_source.changes'
-echo 'sudo dput ppa:pritunl/pritunl-testing ./build/openvpn_'$VERSION'-0ubuntu1~precise_source.changes'
-echo 'sudo dput ppa:pritunl/pritunl-testing ./build/openvpn_'$VERSION'-0ubuntu1~utopic_source.changes'
+echo 'sudo dput ppa:pritunl/pritunl-dev ./build/openvpn_'$VERSION'-0ubuntu1~trusty_source.changes'
+echo 'sudo dput ppa:pritunl/pritunl-dev ./build/openvpn_'$VERSION'-0ubuntu1~precise_source.changes'
